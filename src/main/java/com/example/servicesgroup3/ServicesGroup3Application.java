ef5e508f874ab981fbe81c8aff5e77a403e070bd
Nhat180 <s3740975@rmit.edu.vn>
2021-12-24 11:25:38 +0700

    First Commit

---
+++ src/main/java/com/example/servicesgroup3/ServicesGroup3Application.java
@@ -0,0 +1,13 @@
+package com.example.servicesgroup3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesGroup3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesGroup3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/servicesgroup3/ServicesGroup3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servicesgroup3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesGroup3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
