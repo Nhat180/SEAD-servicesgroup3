eb6cac9282eba9c7fc3b55004604d644708e1dd9
Nhat180 <s3740975@rmit.edu.vn>
2021-12-27 23:51:43 +0700

    Create Services.java

---
+++ src/main/java/com/example/servicesgroup3/model/Services.java
@@ -0,0 +1,27 @@
+package com.example.servicesgroup3.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Services {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long serviceId;
+
+    private String name;
+
+    private Integer cost;
+
+    private Double rating;
+
+}
